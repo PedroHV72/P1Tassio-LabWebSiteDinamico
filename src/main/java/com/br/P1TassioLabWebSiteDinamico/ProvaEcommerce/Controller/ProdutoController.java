8a249f02708efa835c51a39bb089f2abd6c26f3e
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2021-09-26 01:09:59 -0300

    Foi feito o Controller e as primeiras requisições http com metodo POST e GET

---
+++ src/main/java/com/br/P1TassioLabWebSiteDinamico/ProvaEcommerce/Controller/ProdutoController.java
@@ -0,0 +1,32 @@
+package com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Controller;
+
+import com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Model.Entity.Produto;
+import com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Model.Repository.ProdutoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(JdbcTemplate jdbcTemplate) {
+        produtoRepository = new ProdutoRepository(jdbcTemplate);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Produto inserir(@RequestBody Produto produto) throws Exception {
+        return produtoRepository.adicionar(produto);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public ArrayList<Produto> buscarPorId(@PathVariable Integer id) throws Exception {
+        return produtoRepository.buscarPorId(id);
+    }
+}
