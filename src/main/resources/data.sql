e0db49aebfb9b4c735920e1324e5d4291c25e387
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2021-09-26 00:07:22 -0300

    Foi criado o banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO categoria(id, imagemSimboloUrl, nome, descricao)
+VALUES (1, 'https://i.pinimg.com/280x280_RS/f8/7c/bd/f87cbdcdd70d1e4fa3f6164f50d8e023.jpg', 'Whey', 'Os melhores suplementos do mercado');
+
+INSERT INTO categoria(id, imagemSimboloUrl, nome, descricao)
+VALUES (2, 'https://i.pinimg.com/280x280_RS/f8/7c/bd/f87cbdcdd70d1e4fa3f6164f50d8e023.jpg', 'Creatina', '');
+
+INSERT INTO categoria(id, imagemSimboloUrl, nome, descricao)
+VALUES (3, 'https://www.dicasdetreino.com.br/wp-content/uploads/2018/02/Entenda-o-que-%C3%A9-o-Durateston-para-que-serve-benef%C3%ADcios-fun%C3%A7%C3%A3o-excesso-efeitos-colaterais-como-tomar.jpg', 'Durateston', 'Bomba');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE categoria(
+      id                  INT            NOT NULL,
+      ImagemSimboloUrl    VARCHAR (1000),
+      nome                VARCHAR (40)   NOT NULL,
+      descricao           VARCHAR (300)
+);
