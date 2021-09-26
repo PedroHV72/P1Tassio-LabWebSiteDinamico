0684dedaf6dca78f85ce14ad9722632f5b0975c3
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2021-09-26 15:54:01 -0300

    Foi criado novas requisicoes http de metodo GET, buscando produtos de tal categoria e buscando por nome ou valor minimo e maximo de um produto de uma tal categoria

--- src/main/java/com/br/P1TassioLabWebSiteDinamico/ProvaEcommerce/Controller/CategoriaController.java
+++ src/main/java/com/br/P1TassioLabWebSiteDinamico/ProvaEcommerce/Controller/CategoriaController.java
@@ -2,12 +2,14 @@ package com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Controller;
 
 
 import com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Model.Entity.Categoria;
+import com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Model.Entity.Produto;
 import com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Model.Repository.CategoriaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categoria")
@@ -22,5 +24,13 @@ public class CategoriaController {
     @GetMapping("/{id}")
     public ArrayList<Categoria> buscarPorId(@PathVariable Integer id) throws Exception {
         return categoriaRepository.buscarPorId(id);
+    }
+
+    @GetMapping("/{id}/produto")
+    public List<Produto> buscarProdutosCategoria(
+            @PathVariable int id,
+            @RequestParam(required = false) String nome, Float valorMinimo, Float valorMaximo
+    ) {
+        return categoriaRepository.buscarProdutosCategoria(id, nome, valorMinimo, valorMaximo);
     }
 }
--- src/main/java/com/br/P1TassioLabWebSiteDinamico/ProvaEcommerce/Model/Repository/CategoriaRepository.java
+++ src/main/java/com/br/P1TassioLabWebSiteDinamico/ProvaEcommerce/Model/Repository/CategoriaRepository.java
@@ -1,47 +1,16 @@
 package com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Model.Repository;
 
 import com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Model.Entity.Categoria;
+import com.br.P1TassioLabWebSiteDinamico.ProvaEcommerce.Model.Entity.Produto;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriaRepository {
     private JdbcTemplate jdbcTemplate;
 
     public CategoriaRepository(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
-
-    public ArrayList<Categoria> buscarListaCategoria(Integer id,
-                                                     String imagemSimboloUrl,
-                                                     String nome,
-                                                     String descricao)
-        throws Exception {
-
-            String sql = "SELECT * FROM categoria";
-            ArrayList<Categoria> buscar = (ArrayList<Categoria>) jdbcTemplate.query(sql, new Object[]{id, imagemSimboloUrl, nome, descricao}, new CategoriaMapper());
-
-            if(buscar.size() > 0) {
-                return (ArrayList<Categoria>) jdbcTemplate.query(sql, new Object[]{id, imagemSimboloUrl, nome, descricao}, new CategoriaMapper());
-            }
-
-            throw new Exception("Categoria não encontrada");
-
-    }
-
-    public Categoria adicionar(Categoria categoria) throws Exception {
-        String sql = "insert into categoria(id, imagemSimboloUrl, nome, descricao) values (?, ?, ?, ?)";
-        int insert = jdbcTemplate.update(sql,
-                categoria.getId(),
-                categoria.getImagemSimboloUrl(),
-                categoria.getNome(),
-                categoria.getDescricao()
-        );
-
-        if(insert == 1) {
-            return categoria;
-        }
-
-        throw new Exception("Erro ao inserir a categoria");
-    }
 
     public ArrayList<Categoria> buscarPorId(Integer id) throws Exception {
         String sql = "SELECT * FROM categoria WHERE id = ?";
@@ -51,5 +20,31 @@ public class CategoriaRepository {
             return (ArrayList<Categoria>) jdbcTemplate.query(sql, new Object[]{id}, new CategoriaMapper());
         }
         throw new Exception("Nenhuma categoria encontrada com este ID.");
+    }
+
+    public List<Produto> buscarProdutosCategoria(Integer id, String nome, Float valorMinimo, Float valorMaximo) {
+        if (nome == null && valorMinimo == null && valorMaximo == null) {
+            return jdbcTemplate.query(
+                    "select p.*, pc.categoriaId FROM produto p INNER JOIN produtoCategoria pc ON pc.produtoId = p.id WHERE pc.categoriaId = ?",
+                    new ProdutoMapper(),
+                    id
+            );
+        }
+        if (nome != null && valorMinimo == null && valorMaximo == null) {
+            return jdbcTemplate.query(
+                    "select p.* FROM produto p INNER JOIN produtoCategoria pc ON pc.produtoId = p.id WHERE pc.categoriaId = ? AND p.nome = ?",
+                    new ProdutoMapper(),
+                    id,
+                    nome
+            );
+        }
+        return jdbcTemplate.query(
+                "select p.*, pc.categoriaId FROM produto p INNER JOIN produtoCategoria pc ON pc.produtoId = p.id WHERE pc.categoriaId = ? AND p.nome = ? AND p.valorUnitario >= ? AND p.valorUnitario <= ?",
+                new ProdutoMapper(),
+                id,
+                nome,
+                valorMinimo,
+                valorMaximo
+        );
     }
 }
